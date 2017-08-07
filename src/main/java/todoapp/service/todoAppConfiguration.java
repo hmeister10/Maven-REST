cf84f8dc7729e81244e8cadafc7761a753798624
Hiranandani, Himanshu (US - Bengaluru) <hhiranandani@DELOITTE.com>
2017-08-07 12:30:21 +0530

    initial commit

---
+++ src/main/java/todoapp/service/todoAppConfiguration.java
@@ -0,0 +1,13 @@
+package todoapp.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class todoAppConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(todoAppConfiguration.class, args);
+    }
+
+}
