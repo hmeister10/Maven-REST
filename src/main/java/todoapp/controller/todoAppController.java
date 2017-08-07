e8eb68ad61864f77ca4b7d16272d12e6fcd582aa
Hiranandani, Himanshu (US - Bengaluru) <hhiranandani@DELOITTE.com>
2017-08-07 16:35:09 +0530

    removes the delete mapping

--- src/main/java/todoapp/controller/todoAppController.java
+++ src/main/java/todoapp/controller/todoAppController.java
@@ -3,10 +3,7 @@ package todoapp.controller;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.test.web.client.ResponseActions;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,9 +34,4 @@ public class todoAppController {
          }
           
         }
-
-    @RequestMapping(method=RequestMethod.DELETE)
-    public @ResponseBody ResponseEntity<Void> removeFromList(
-        return new ResponseEntity<Void>(HttpStatus.OK);
-    )
 }
